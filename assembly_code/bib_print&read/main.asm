c77ac5322ef71a546c94ae98c9e5f0ab371ceac1
Guilherme Bernardo <bguilherme51@gmail.com>
2022-01-16 14:23:12 -0400

    ativ 1 finished

--- assembly_code/bib_print&read/main.asm
+++ assembly_code/bib_print&read/main.asm
@@ -1,47 +1,56 @@
-section .data 
-message: db "Olá Alunos!", 10, 0
-msgInput: db "Digite um caracter: ", 0
-msgInput_1: db "Digite uma palavra: ", 0
-msgOutput: db "O caracter digitado foi: ", 0
-msgOutput_1: db "A palavra digitada foi: ", 0
+section .data
+message: db "Salve professor!!!", 10, 0
+str1: db 'teste1',0
+str2: db 'teste1',0
+arg1: db 'copiar',10,0
+arg2: times '1' db  66
+num: db '301'
+negNum: db '-540'
 
 section .text
 %include "lib.ioString"
 global _start
-
+ ;testing all the functions in lib.ioString
 _start:
-  	mov rdi, message
-  	call print_string
-  	call print_newline
+	;print string
+	mov rdi, message 
+	call print_string
+	;print unsigned int
 
-  	mov rdi, msgInput
-  	call print_string
-  	call read_char  
-  	push rax
-  
-  	mov rdi, msgOutput
-  	call print_string
-  	pop  rdi
-  	call print_char  
-  	call print_newline
-    call print_newline
-    
-    mov rdi, msgInput_1
-  	call print_string    
-    
-  	push 0  ; alocando memória
-    push 0  ; alocando memória
-    mov rdi, rsp ; endereço do inicio do buffer
-    mov rsi, 16  ; length to buffer
-    call read_word
-    push rax
-    mov rdi, msgOutput_1
-  	call print_string
-    pop rax
-    mov rdi, rax
-    push rdx 		; caller saving string length
-    call print_string
-    call print_newline
-    call print_newline
+	mov rax, 09410948
+	call print_uint
+	;print unsigned int
 
-  	call exit
+	mov rdi, -2012901
+	call print_int
+	;parse an int and print it
+
+	mov rdi, num
+	call parse_uint
+	mov rdi,rax
+	call print_uint
+	;parse an int and print it
+
+	mov rdi, negNum
+	call parse_int
+	mov rdi,rax
+	call print_int
+	;parse an int and print it
+
+	mov rdi, str1
+	mov rsi, str2
+	call string_equals
+	;compare strings
+
+	mov rdi, rax
+	call print_uint
+
+	mov rdi, arg1
+	mov rsi, arg2
+	call string_copy
+	;copy strings
+	mov rdi, arg2 
+	call print_string
+	;print string
+
+	call exit
